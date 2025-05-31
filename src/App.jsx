6a8e8c77769f7e3bfa701a59d343afc0f6c90d01
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-05-31 22:25:59 +0530

    refactor: component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import "./App.css";
 import SpendLogger from "./components/SpendLogger";
 
 function App() {
-  return (
-    <div className="p-4 max-w-md mx-auto">
-      <h2 className="text-center text-xl mb-4">💸 My Spend Logger</h2>
-
-      <main>
-        <SpendLogger />
-      </main>
-    </div>
-  );
+  return <SpendLogger />;
 }
 
 export default App;
